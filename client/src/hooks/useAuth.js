5fd13130beded8fba9d1ae2be8394d68372ce2b6
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-07-27 19:11:16 +0300

    Created useAuth hook

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,42 @@
+import { login, logout, register } from "../api/auth-api";
+import { useAuthContext } from "../contexts/AuthContext"
+
+export const useLogin = () => {
+    const { changeAuthState } = useAuthContext();
+
+    const loginHandler = async (email, password) => {
+        const {password: _, ...authData} = await login(email, password);
+
+        changeAuthState(authData);
+
+        return authData;
+    }
+
+    return loginHandler;
+}
+
+export const useRegister = () => {
+    const {changeAuthState} = useAuthContext();
+
+    const registerHandler = async (email, password) => {
+        const {password: _, ...authData} = await register(email, password);
+
+        changeAuthState(authData);
+
+        return authData;
+    }
+
+    return registerHandler;
+}
+
+export const useLogout = () => {
+    const {logout: localLogout} = useAuthContext();
+
+    const logoutHandler = async () => {
+        await logout();
+
+        localLogout();
+    }
+
+    return logoutHandler;
+}
