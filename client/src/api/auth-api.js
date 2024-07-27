6735a598a492ffcabd1d1e0294347b47d525946d
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-07-27 19:06:37 +0300

    Added auth api

---
+++ client/src/api/auth-api.js
@@ -0,0 +1,9 @@
+import requester from "./requester";
+
+const BASE_URL = 'http://localhost:3030/users';
+
+export const login = (email, password) => requester.post(`${BASE_URL}/login`, {email, password});
+
+export const register = (email, password) => requester.post(`${BASE_URL}/register`, {email, password});
+
+export const logout = () => requester.get(`${BASE_URL}/logout`);
