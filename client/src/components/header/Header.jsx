0cbca195093133b8d2bd5ead65aff7e389c8e7be
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-07-27 19:16:22 +0300

    Added dynamic navigation

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,11 +1,18 @@
+import withAuth from '../../HOC/withAuth';
+import { useAuthContext } from '../../contexts/AuthContext';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
 
-export default function Header() {
+function Header({
+    auth
+}) {
+    //const { isAuthenticated } = useAuthContext(); {/* Without HOC */}
+    const {isAuthenticated} = auth;
+
     return (
         <header>
             <Link to="/" className={styles.logo}>
-            <img src="https://www.pngall.com/wp-content/uploads/8/Vector-Travel-PNG-Free-Download.png" className={styles['logo-img']} alt="Travel Logo" />
+                <img src="https://www.pngall.com/wp-content/uploads/8/Vector-Travel-PNG-Free-Download.png" className={styles['logo-img']} alt="Travel Logo" />
             </Link>
             <div className={styles['bx bx-menu']} id="menu-icon" />
             <ul className={styles.navbar}>
@@ -24,25 +31,36 @@ export default function Header() {
                 <li>
                     <Link to="/testimonials">Testimonials</Link>
                 </li>
-                {/**Guest */}
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/register">Register</Link>
-                </li>
-                {/**User */}
-                <li>
-                    <Link to="/vacations/create">Create Vacation</Link>
-                </li>
-                <li>
-                    <Link to="/profile">Profile</Link>
-                </li>
-                <li>
-                    <Link to="/logout">Logout</Link>
-                </li>
+                {isAuthenticated
+                    ? (
+                        <>
+                            <li>
+                                <Link to="/vacations/create">Create Vacation</Link>
+                            </li>
+                            <li>
+                                <Link to="/profile">Profile</Link>
+                            </li>
+                            <li>
+                                <Link to="/logout">Logout</Link>
+                            </li>
+                        </>)
+                    : (
+                        <>
+                            <li>
+                                <Link to="/login">Login</Link>
+                            </li>
+                            <li>
+                                <Link to="/register">Register</Link>
+                            </li>
+                        </>
+                    )}
+
             </ul>
         </header>
 
     );
 }
+
+const EnhancedHeader = withAuth(Header);
+
+export default EnhancedHeader;
