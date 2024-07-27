c42c06a0e1ab876d758a0435c15b1a779eb5b395
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-07-27 19:05:37 +0300

    Added requester

---
+++ client/src/api/requester.js
@@ -0,0 +1,54 @@
+import { getAccessToken } from "../utils/authUtils";
+
+async function requester(method, url, data) {
+    const options = {};
+
+    const accessToken = getAccessToken();
+
+    if(accessToken) {
+        options.headers = {
+            ...options.headers,
+            'X-Authorization': accessToken
+        }
+    }
+
+    if(method !== 'GET') {
+        options.method = method;
+    }
+
+    if(data) {
+        options.headers = {
+            ...options.headers,
+            'Content-Type': 'application/json'
+        };
+
+        options.body = JSON.stringify(data);
+    }
+
+    const response = await fetch(url, options);
+
+    if(response.status === 204) {
+        return;
+    }
+
+    const result = await response.json();
+
+    if(!response.ok) {
+        throw result;
+    }
+
+    return result;
+
+}
+
+export const get = requester.bind(null, 'GET');
+export const post = requester.bind(null, 'POST');
+export const put = requester.bind(null, 'PUT');
+export const del = requester.bind(null, 'DELETE');
+
+export default {
+    get,
+    post,
+    put,
+    del
+}
