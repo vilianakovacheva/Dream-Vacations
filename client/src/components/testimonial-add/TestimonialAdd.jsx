07f90094db678ff22de77f1c9228967350aa4492
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-07-28 16:40:53 +0300

    Created add testimonial functionality

--- client/src/components/testimonial-add/TestimonialAdd.jsx
+++ client/src/components/testimonial-add/TestimonialAdd.jsx
@@ -1,6 +1,34 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './TestimonialAdd.module.css'
+import { useCreateTestimonial } from '../../hooks/useTestimonials';
+import { useState } from 'react';
+import { useForm } from '../../hooks/useForm';
+
+const initialValues = {
+    testimonial: ''
+}
 
 export default function TestimonialAdd() {
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+    const createTestimonial = useCreateTestimonial();
+
+    const createHandler = async (values) => {
+        if (!values.testimonial) {
+            setError('Please write your testimonial!');
+            return;
+        }
+
+        try {
+            await createTestimonial(values);
+            navigate('/testimonials');
+        } catch (err) {
+            setError(err.message);
+        }
+    }
+
+    const { values, changeHandler, submitHandler } = useForm(initialValues, createHandler);
+
     return (
         <>
             <h1 className={styles["testimonial-heading"]}>Add Testimonial:</h1>
@@ -9,14 +37,25 @@ export default function TestimonialAdd() {
                 <br />
                 Are you satisfied with us and our organization?
             </p>
-            <div className={styles["testimonial-container"]}>
+            <form className={styles["testimonial-container"]} onSubmit={submitHandler}>
                 <textarea
                     className={styles["testimonial-input"]}
+                    name="testimonial"
+                    value={values.testimonial}
+                    onChange={changeHandler}
                     placeholder="Write your testimonial..."
-                    defaultValue={""}
                 />
-                <button className={styles["add-testimonial-btn"]}>Add Testimonial</button>
-            </div>
+
+                {error && (
+                    <p>
+                        <span style={{ fontSize: '18px', color: 'red' }}>
+                            {error}
+                        </span>
+                    </p>
+                )}
+
+                <input className={styles["add-testimonial-btn"]} type="submit" value="Add Testimonial" />
+            </form>
         </>
 
 
