d35e58bb895174b73e384a05614fec1bd048c47a
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-07-28 14:17:55 +0300

    Added details page for each vacation

--- client/src/components/vacation-details/VacationDetails.jsx
+++ client/src/components/vacation-details/VacationDetails.jsx
@@ -1,47 +1,56 @@
+import { useParams } from 'react-router-dom';
 import styles from './VacationDetails.module.css'
+import { useAuthContext } from '../../contexts/AuthContext';
+import { useGetOneVacation } from '../../hooks/useVacations';
 
 export default function VacationDetails() {
+    const { vacationId } = useParams();
+    const { userId } = useAuthContext();
+    const [vacation] = useGetOneVacation(vacationId);
+    const { isAuthenticated } = useAuthContext();
+
+    const isOwner = userId === vacation._ownerId;
+
     return (
         <section className={styles['vacation-details']}>
-            <h1 className={styles.destination}>PARIS</h1>
+            <h1 className={styles.destination}>{vacation.destination}</h1>
             <img
-                src="https://i.pinimg.com/originals/35/33/ad/3533adba2e1983f7c8b9809e1d2b82ea.jpg"
+                src={vacation.imageUrl}
                 className={styles["destination-img"]}
                 alt=""
             />
             <div className={styles["first-description"]}>
-                <p className={styles["first-description-p"]}>💵 Price: $1630</p>
-                <p className={styles["first-description-p"]}>📍 Destination: Paris</p>
-                <p className={styles["first-description-p"]}>🏨 Accommodation: Altona</p>
-                <p className={styles["first-description-p"]}>🌟 Stars: 4-star</p>
-                <p className={styles["first-description-p"]}>🌑 Period: 7 nights</p>
+                <p className={styles["first-description-p"]}>💵 Price: ${vacation.price}</p>
+                <p className={styles["first-description-p"]}>📍 Destination: {vacation.destination}</p>
+                <p className={styles["first-description-p"]}>🏨 Accommodation: {vacation.accommodation}</p>
+                <p className={styles["first-description-p"]}>🌟 Stars: {vacation.stars}-star</p>
+                <p className={styles["first-description-p"]}>🌑 Period: {vacation.period} nights</p>
             </div>
             <div className={styles["second-description"]}>
                 <h3 className={styles["heading"]}>Description of the accommodation</h3>
                 <p className={styles["description-paragraph"]}>
-                    Quiet Paris, practical Paris, this is what the Altona hotel offers you,
-                    ideally located near the Gare du Nord and Gare de l'Est, a few minutes
-                    from Place de la République. The Sacré-Coeur basilica and Montmartre are
-                    also within walking distance of the hotel. In short, Paris is at your
-                    feet! In a beautiful building from the beginning of the century, the
-                    Altona hotel offers you a personalized welcome!
+                    {vacation.description}
                 </p>
                 <h3 className={styles['heading']}>What you can do there:</h3>
                 <p className={styles["activities-paragraph"]}>
-                    You can visit the Eiffel Tower, Louvre Museum, Arc de Triomphe, Palace of
-                    Versailles.
+                    {vacation.entertainment}
                 </p>
             </div>
-            <p className={styles.likes}>👍🏼 Likes: 3</p>
-            <div className={styles["author-buttons"]}>
-                <button id="edit-button">Edit</button>
-                <button id="delete-button">Delete</button>
-            </div>
-            <div className={styles["nonauthor-button"]}>
-                <button id="like-button">Like</button>
-            </div>
-            <p className={styles["liked-vacation"]}>You already liked this vacation!</p>
-            </section>
+            {/*<p className={styles.likes}>👍🏼 Likes: 3</p>*/}
+            {isOwner && (
+                <div className={styles["author-buttons"]}>
+                    <button id="edit-button">Edit</button>
+                    <button id="delete-button">Delete</button>
+                </div>
+            )}
+
+            {isAuthenticated && !isOwner && (
+                <div className={styles["nonauthor-button"]}>
+                    <button id="like-button">Like</button>
+                </div>
+            )}
+            {/*<p className={styles["liked-vacation"]}>You already liked this vacation!</p>*/}
+        </section>
 
     );
 }
