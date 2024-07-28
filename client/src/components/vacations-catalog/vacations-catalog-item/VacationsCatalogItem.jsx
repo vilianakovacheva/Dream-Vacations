1b6a04159be1b6f4f3af8bbe001e7a2adba72227
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-07-28 16:37:50 +0300

    Changed the name of the module css for the vacations catalog

--- client/src/components/vacations-catalog/vacations-catalog-item/VacationsCatalogItem.jsx
+++ client/src/components/vacations-catalog/vacations-catalog-item/VacationsCatalogItem.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import styles from './VacationsListItem.module.css';
+import styles from './VacationsCatalogItem.module.css';
 
 export default function VacationListItem({
     _id,
