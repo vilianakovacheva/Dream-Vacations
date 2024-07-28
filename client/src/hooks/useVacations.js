d8f4c0e3622902fafc0d77efadc96b2870ba63a8
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-07-28 13:38:13 +0300

    Created useVacations hook

---
+++ client/src/hooks/useVacations.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import * as vacationsAPI from '../api/vacations-api';
+
+export function useGetAllVacations() {
+    const [vacations, setVacations] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const result = await vacationsAPI.getAll();
+
+            setVacations(result);
+        })();
+    }, []);
+
+    return [vacations, setVacations];
+}
+
+export function useGetOneVacation(vacationId) {
+    const [vacation, setVacation] = useState({});
+
+    useEffect(() => {
+        (async () => {
+            const result = await vacationsAPI.getOne(vacationId);
+
+            setVacation(result);
+        })();
+    }, [vacationId]);
+
+    return [vacation, setVacation];
+}
+
+export function useCreateVacation(vacationData) {
+    const vacationCreateHandler = (vacationData) => vacationsAPI.create(vacationData);
+
+    return vacationCreateHandler;
+}
